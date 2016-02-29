c5ca45663a7413208ad0788b48b5bc3ebbc06684
taapesh <starmansix@gmail.com>
2016-02-29 03:22:23 -0600

    Refactoring and added about page

--- app/apps.py
+++ app/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 
 
 class PersonalAppConfig(AppConfig):
-    name = 'personal_app'
+    name = 'app'
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
-from personal_app import views
+from app import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
+    url(r'^about', views.about, name='about'),
 ]
