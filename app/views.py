d486a0b43de251046413961075281c167af9cbdf
taapesh <starmansix@gmail.com>
2016-02-29 10:56:48 -0600

    Big redesign

--- app/views.py
+++ app/views.py
@@ -3,8 +3,15 @@ from django.shortcuts import render
 def home(request):
     return render(request, "home.html", {})
 
+def blog(request):
+    return render(request, "blog.html", {})
+
 def about(request):
     return render(request, "about.html", {})
 
+def projects(request):
+    return render(request, "projects.html", {})
+
 def learn(request):
     return render(request, "learn.html", {})
+    
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,6 +5,8 @@ from app import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
+    url(r'^blog', views.blog, name='blog'),
     url(r'^about', views.about, name='about'),
+    url(r'^projects', views.projects, name='projects'),
     url(r'^learn', views.learn, name='learn'),
 ]
