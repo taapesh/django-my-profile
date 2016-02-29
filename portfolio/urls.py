d341b2facbdb371e128f5268faafe5c0fc40f023
taapesh <starmansix@gmail.com>
2016-02-29 04:37:19 -0600

    Added useful links

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
     url(r'^about', views.about, name='about'),
+    url(r'^learn', views.learn, name='learn'),
 ]
